25a7936884a9fb7c1af6f99d764b381a6b3c73f5
Samuel Jansen <samuel.jansen@cwi.com.br>
2020-04-05 17:36:38 -0300

    refact(fix-globals-structure): just refactoring api so that it fits to its description

--- api/src/GlobalsApplication.py
+++ api/src/GlobalsApplication.py
@@ -1,16 +1,19 @@
 from domain.control import PathMannanger
 pathMannanger = PathMannanger.PathMannanger()
 
-print('glopals api')
+print('Glopals api')
 
 print(
 f'''pathMannanger = {pathMannanger}
-pathMannanger.currentPath =                             {pathMannanger.currentPath}
-pathMannanger.localPath =                               {pathMannanger.localPath}
-pathMannanger.apiName =                                 {pathMannanger.apiName}
-pathMannanger.apiPath =                                 {pathMannanger.apiPath}
-pathMannanger.baseApiPath =                             {pathMannanger.baseApiPath}
-pathMannanger.apiModules =                              {pathMannanger.apiModules}
-pathMannanger.importMannangerRootInsideBaseApiPath =    {pathMannanger.importMannangerRootInsideBaseApiPath}
-pathMannanger.globalsModule =                           {pathMannanger.globalsModule}
-pathMannanger.globalsModulePath =                       {pathMannanger.globalsModulePath}''')
+pathMannanger.currentPath =                 {pathMannanger.currentPath}
+pathMannanger.localPath =                   {pathMannanger.localPath}
+pathMannanger.baseApiPath =                 {pathMannanger.baseApiPath}
+pathMannanger.apiPath =                     {pathMannanger.apiPath}
+pathMannanger.apiName =                     {pathMannanger.apiName}
+pathMannanger.apisRoot =                    {pathMannanger.apisRoot}
+pathMannanger.apiNames =                    {pathMannanger.apiNames}
+pathMannanger.localGlobalsApiPath =         {pathMannanger.localGlobalsApiPath}
+pathMannanger.globalsApiName =              {pathMannanger.globalsApiName}
+pathMannanger.globalsApiPath =              {pathMannanger.globalsApiPath}''')
+
+import Application
--- api/src/domain/control/PathMannanger.py
+++ api/src/domain/control/PathMannanger.py
@@ -9,24 +9,16 @@ print('PathMannanger library imported')
 class PathMannanger:
 
     BASE_API_PATH = 'api\\src\\'
-    API_NAME = 'Courses'
+    LOCAL_GLOBALS_API_PATH = 'domain\\control\\'
     EXTENSION = 'ht'
-    GLOBALS = 'globals'
-    COURSE = 'course'
-    APPLICATION = 'application'
-    DESKTOP = 'desktop'
-    EDITOR = 'editor'
+    GLOBALS = 'Globals'
 
-    API_MODULES = [
+    API_LIST = [
         GLOBALS,
-        COURSE,
-        APPLICATION,
-        DESKTOP,
-        EDITOR
+        'Application'
     ]
 
     NODE_IGNORE = [
-        ###- 'resourse',
         'image',
         'audio',
         '__pycache__',
@@ -41,47 +33,48 @@ class PathMannanger:
         self.currentPath = str(Path(__file__).parent.absolute())
         self.localPath = f'{str(Path.home())}\\'
 
-        self.apiName = PathMannanger.API_NAME
         self.baseApiPath = PathMannanger.BASE_API_PATH
-        self.apiPath = f'{self.currentPath.split(self.apiName)[0]}{self.apiName}\\'
-        self.apiModules = PathMannanger.API_MODULES
+        self.apiPath = self.currentPath.split(self.baseApiPath)[0]
+        self.apiName = self.apiPath.split('\\')[-2]
+        self.apisRoot = self.currentPath.split(self.localPath)[1].split(self.apiName)[0]
+        self.apiNames = PathMannanger.API_LIST
 
-        self.importMannangerRootInsideBaseApiPath = 'domain\\control\\PathMannanger.py'
-        self.globalsModule = PathMannanger.GLOBALS
-        self.globalsModulePath = f'{self.getApiModulePath(self.globalsModule)}{self.importMannangerRootInsideBaseApiPath}'
+        self.globalsApiName = PathMannanger.GLOBALS
+        self.localGlobalsApiPath = f'{PathMannanger.LOCAL_GLOBALS_API_PATH}{PathMannanger.__name__}.py'
+        self.globalsApiPath = f'{self.getApiPath(self.globalsApiName)}{self.localGlobalsApiPath}'
 
         self.backSlash = PathMannanger.BACK_SLASH
 
         self.update()
-        self.newApplicationHandler()
+        ###- self.newApplicationHandler()
 
-    def getApiModulePath(self,apiModuleName):
-        return f'{self.apiPath}{apiModuleName}\\{self.baseApiPath}'
+    def getApiPath(self,apiName):
+        return f'{self.localPath}{self.apisRoot}{apiName}\\{self.baseApiPath}'
 
     def update(self) :
         # https://stackoverflow.com/questions/3430372/how-do-i-get-the-full-path-of-the-current-files-directory
         # from pathlib import Path
         globalsScript = []
-        with open(self.globalsModulePath,"r",encoding="utf-8") as globalsFile :
+        with open(self.globalsApiPath,"r",encoding="utf-8") as globalsFile :
             for line in globalsFile :
                 globalsScript.append(line)
 
-        for apiModule in self.apiModules :
-            updatingModulePath =f'{self.getApiModulePath(apiModule)}{self.importMannangerRootInsideBaseApiPath}'
-            if apiModule != self.globalsModule :
-                with open(updatingModulePath,"w+",encoding="utf-8") as moduleFile :
-                    moduleFile.write(''.join(globalsScript))
+        for apiName in self.apiNames :
+            updatingApiPath =f'{self.getApiPath(apiName)}{self.localGlobalsApiPath}'
+            if apiName != self.globalsApiName :
+                with open(updatingApiPath,"w+",encoding="utf-8") as PathMannangerFile :
+                    PathMannangerFile.write(''.join(globalsScript))
 
         # https://stackabuse.com/how-to-copy-a-file-in-python/
         # import shutil
         # shutil.copy2('file1.txt', 'file2.txt')
-        self.makeAplicationLibrariesAvaliable()
 
-    def makeAplicationLibrariesAvaliable(self) :
+        self.makeApisAvaliable()
 
+    def makeApisAvaliable(self) :
         self.modulesNodeTree = []
-        for moduleName in PathMannanger.API_MODULES :
-            self.modulesNodeTree.append(self.makeLibraryPathTreeAvaliable(self.getApiModulePath(moduleName)))
+        for apiName in PathMannanger.API_LIST :
+            self.modulesNodeTree.append(self.makePathTreeAvaliable(self.getApiPath(apiName)))
         print(f'PathMannanger.modulesNodeTree = {self.modulesNodeTree}')
 
     def getPathTreeFromPath(self,path):
@@ -95,14 +88,14 @@ class PathMannanger:
                 except : pass
         return node
 
-    def makeLibraryPathTreeAvaliable(self,path):
+    def makePathTreeAvaliable(self,path):
         node = {}
         nodeSons = os.listdir(path)
         for nodeSon in nodeSons :
             if self.nodeIsValid(nodeSon) :
                 nodeSonPath = f'{path}\\{nodeSon}'
                 try :
-                    node[nodeSon] = self.makeLibraryPathTreeAvaliable(nodeSonPath)
+                    node[nodeSon] = self.makePathTreeAvaliable(nodeSonPath)
                 except : pass
         sys.path.append(path)
         return node
@@ -113,6 +106,6 @@ class PathMannanger:
     def getExtension(self):
         return PathMannanger.EXTENSION
 
-    def newApplicationHandler(self):
-        import ApplicationHandler
-        self.applicationHandler = ApplicationHandler.ApplicationHandler(self)
+    # def newApplicationHandler(self):
+    #     import ApplicationHandler
+    #     self.applicationHandler = ApplicationHandler.ApplicationHandler(self)
