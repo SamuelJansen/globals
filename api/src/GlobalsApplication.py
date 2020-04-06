caf47f246c4aab2b4935187e75fb873d665b611f
Samuel Jansen <samuel.jansen@cwi.com.br>
2020-04-06 00:25:35 -0300

    feat(fast-way-to-put-it-up): far away from propper way to use it

--- api/src/GlobalsApplication.py
+++ api/src/GlobalsApplication.py
@@ -1,9 +1,7 @@
-from domain.control import PathMannanger
-pathMannanger = PathMannanger.PathMannanger(
-    mode = 'WRONG_WAY_TO_MAKE_IT_WORKS',
-    printStatus = True
-)
-# pathMannanger = PathMannanger.PathMannanger(printStatus = True)
+from api.src.domain.control import PathMannanger
+if __name__ == '__main__' :
+    PathMannanger.PathMannanger(mode = 'WRONG_WAY_TO_MAKE_IT_WORKS')
+    # PathMannanger.PathMannanger(printStatus = True)
 
 print('Glopals api')
 
