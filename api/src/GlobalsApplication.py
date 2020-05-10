3e62be3331ef1ab28af5d356b4bd574fd2996fe6
Samuel Jansen <samuel.jansen@cwi.com.br>
2020-05-10 16:04:40 -0300

    feat(settings-loadder-update):

--- api/src/GlobalsApplication.py
+++ api/src/GlobalsApplication.py
@@ -1,4 +1,7 @@
 if __name__ == '__main__' :
-    from domain.control import PathMannanger
-    # PathMannanger.PathMannanger(mode = 'WRONG_WAY_TO_MAKE_IT_WORKS')
-    PathMannanger.PathMannanger(globalsApis='Globals.api.list',printStatus = True)
+    from domain.control import Globals
+    Globals.Globals(
+        mode = Globals.PROPER_WAY_TO_IMPLEMENT_IT,
+        encoding = Globals.ENCODING,
+        debugStatus = True
+    )
