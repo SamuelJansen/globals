71278a12a36bc969bbe2e0a35b963f5b4b0a6cbd
Samuel Jansen <samuel.jansen@cwi.com.br>
2020-07-03 00:49:27 -0300

    feat: globals package distribution

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 setup(
   name = 'globals',
   packages = ['globals'],
-  version = '0.0.3',
+  version = '0.0.4',
   license='MIT',
   description = 'import package handler',
   author = 'Samuel Jansen',
   author_email = 'samuel.jansenn@gmail.com',
   url = 'https://github.com/SamuelJansen/globals',
-  download_url = 'https://github.com/SamuelJansen/globals/archive/v0.0.3.tar.gz',
+  download_url = 'https://github.com/SamuelJansen/globals/archive/v0.0.4.tar.gz',
   keywords = ['global', 'python global package', 'python package manager'],
   install_requires=[],
   classifiers=[
