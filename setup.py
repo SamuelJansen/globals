77998c71122f4b3d14eac276298df327148e24c5
Samuel Jansen <samuel.jansen@cwi.com.br>
2020-07-03 04:15:37 -0300

    feat: globals package distribution

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from distutils.core import setup
 
-version = '0.0.17'
+version = '0.0.18'
 name = 'globals'
 url = 'https://github.com/SamuelJansen/globals/'
 
 setup(
   name = name,
-  packages = [name, f'{name}.api', f'{name}.api.src'],
+  packages = [name, f'{name}/api', f'{name}/api/src'],
   version = version,
   license='MIT',
   description = 'import package handler',
