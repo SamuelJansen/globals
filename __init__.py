23fb1b9cac3d775fd5251c5a09d74e1b5bf8f613
Samuel Jansen <samuel.jansen@cwi.com.br>
2020-07-03 01:14:47 -0300

    feat: globals package distribution

--- __init__.py
+++ __init__.py
@@ -1,1 +1,7 @@
 
+from globals import Globals
+from globals import Resource
+from globals import ResourceMethod
+from globals import getGlobals
+from globals import getApi
+from globals import addTo
