fa14eab82d0ad43b52f8afa29e6321f982274073
Samuel Jansen <samuel.jansen@cwi.com.br>
2020-07-08 00:04:14 -0300

    feat: package deploy prep

--- setup.py
+++ setup.py
@@ -1,26 +1,31 @@
 from distutils.core import setup
 
-version = '0.0.23'
+version = '0.0.24'
 name = 'globals'
 url = f'https://github.com/SamuelJansen/{name}/'
 
 setup(
-  name = name,
-  packages = [name, f'{name}/api', f'{name}/api/src'],
-  version = version,
-  license='MIT',
-  description = 'import package handler',
-  author = 'Samuel Jansen',
-  author_email = 'samuel.jansenn@gmail.com',
-  url = url,
-  download_url = f'{url}archive/v{version}.tar.gz',
-  keywords = ['global', 'python global package', 'python package manager', 'global package manager'],
-  install_requires=[],
-  classifiers=[
-    'Development Status :: 3 - Alpha',
-    'Intended Audience :: Developers',
-    'Topic :: Software Development :: Build Tools',
-    'License :: OSI Approved :: MIT License',
-    'Programming Language :: Python :: 3.8'
-  ]
+    name = name,
+    packages = [
+        name,
+        f'{name}/api',
+        f'{name}/api/src',
+        f'{name}/api/src/service'
+    ],
+    version = version,
+    license = 'MIT',
+    description = 'import package handler',
+    author = 'Samuel Jansen',
+    author_email = 'samuel.jansenn@gmail.com',
+    url = url,
+    download_url = f'{url}archive/v{version}.tar.gz',
+    keywords = ['global', 'python global package', 'python package manager', 'global package manager'],
+    install_requires = [],
+    classifiers = [
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.8'
+    ]
 )
