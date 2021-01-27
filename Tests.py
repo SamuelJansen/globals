4daf2f2950f56bb54657898bfab8bda06d8e2ce1
Samuel Jansen <>
2021-01-27 01:39:33 -0300

    feat: python_helper updated

--- Tests.py
+++ Tests.py
@@ -1,5 +1,5 @@
 from python_helper import TestHelper
-# TestHelper.run(__file__, inspectGlobals=False)
+TestHelper.run(__file__, inspectGlobals=False)
 TestHelper.run(
     __file__,
     times = 2,
@@ -8,13 +8,13 @@ TestHelper.run(
     logResult = True,
     inspectGlobals = True
 )
-# TestHelper.run(
-#     __file__,
-#     times = 10,
-#     runOnly = [
-#         'MyApplicationTest.startMyApplicationTest'
-#         ],
-#     testStatus = False,
-#     logStatus = False,
-#     logResult = False
-# )
+TestHelper.run(
+    __file__,
+    times = 10,
+    runOnly = [
+        'MyApplicationTest.startMyApplicationTest'
+        ],
+    testStatus = False,
+    logStatus = False,
+    logResult = False
+)
